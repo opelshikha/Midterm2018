1a09fbac68c2f536563db3e70e70a2dc04b7421c
tazrin <tazrinsultanar@gmail.com>
2018-04-10 00:27:41 -0400

    DuplicateWord

--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -1,5 +1,6 @@
 package problems;
-
+import java.util.HashMap;
+import java.util.Map;
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -13,6 +14,23 @@ public class DuplicateWord {
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
+        Map<String, Integer> wrdMap = new HashMap<>();
+        String[] splitWords = st.split(" ");
+
+        for (String word : splitWords){
+            Integer count = wrdMap.get(splitWords);
+            if(count == null){
+                count = 0;
+            }
+            wrdMap.put(word, count+1);
+        }
+
+        for (Map.Entry<String, Integer> s:wrdMap.entrySet()){
+            System.out.println(" " + s.getKey() + " " + s.getValue() + " time(s)");
+        }
+
     }
 
 }
+
+
